0b5b8b58963f8bba09fca65c49646493bf5d682f
MaruthiSarathChandra <106025479+MaruthiSarathChandra@users.noreply.github.com>
2026-09-23 07:48:27 -0500

    Add ATS adapter package

---
+++ browser/adapters/__init__.py
@@ -0,0 +1,9 @@
+from .base import AdapterResult, ApplicationContext, ATSAdapter
+from .registry import get_adapter
+
+__all__ = [
+    "AdapterResult",
+    "ApplicationContext",
+    "ATSAdapter",
+    "get_adapter",
+]
